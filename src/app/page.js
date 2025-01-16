d2292062d52dabd029e7a7dc4245b3dea90471d1
arash-malekzadeh <arash.malekzade@gmail.com>
2025-01-16 12:49:23 +0330

    Link

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h1>welcome to Nextjs course</h1>
+      <Link href={"/products"}> go to poducts page</Link>
+      <Link href={"/accounts"}> go to accounts page</Link>
     </main>
   );
 }
