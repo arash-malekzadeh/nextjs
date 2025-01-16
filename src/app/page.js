8bcec93021ccb364fdc0c019ac579785d7b846a4
arash-malekzadeh <arash.malekzade@gmail.com>
2025-01-16 13:04:43 +0330

    useRouter

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,26 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const Router = useRouter();
+
+  const haandle = () => {
+    Router.push("products");
+  };
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h1>welcome to Nextjs course</h1>
       <Link href={"/products"}> go to poducts page</Link>
       <Link href={"/accounts"}> go to accounts page</Link>
+
+      <h2>using useroute to navigate</h2>
+
+      <button className="text-blue-900" onClick={haandle}>
+        {" "}
+        go to products
+      </button>
     </main>
   );
 }
